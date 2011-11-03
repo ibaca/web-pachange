fb76798ba7a0388575fe35329d53ce7ed486ac56
ignacio.bacamt <ignacio.bacamt@c5f899d2-d125-c12f-7247-f50cb95b8307>
2011-11-03 20:00:38 +0000

    cambios en parte jvmc

--- pachange-web/test/ibaca/test-jvmc/pachange/pachange.js
+++ pachange-web/test/ibaca/test-jvmc/pachange/pachange.js
@@ -1,10 +1,10 @@
 steal(
     'steal/less'                // CSS Less Engine
-    ).then(
+).then(
     '../bootstrap/lib/bootstrap.less'
-    ).then(
+).then(
     'jquery'
-    ).then(
+).then(
     './pachange.css', 		// application CSS file
     './models/models.js',	// steals all your models
     './fixtures/fixtures.js',	// sets up fixtures for your models
@@ -16,47 +16,44 @@ steal(
     'jquery/event/hashchange',
     'jquery/view',
     'jquery/view/ejs',
-    'jquery/view/tmpl',
     'jquery/jsperanto',
     'steal/html',
-    function($){			// configure your application
-        var lang = $.language();
-        var dictionary = $.View('//pachange/locales/'+lang+'.json');
-        
-        // Initialize jSperanto I18n plugin
-        $.jsperanto.init(function(){},{lang: lang, dictionary: dictionary});
-	
-        $('#recipes').pachange_recipe_list();
-        $('#create').pachange_recipe_create();
-        
-        // Initialize topbar
-        $('.topbar').pachange_topbar();
-        
-        $.Controller('Ajaxy',{
-            init : function(){
-                this.updateContent();
-            },
-            "{window} hashchange" : function() {
-                this.updateContent();
-            },
-            updateContent : function(){
-                var hash = window.location.hash.substr(2),
-                    url = "fixtures/"+(hash||"home")+".html";
-                
-                // postpone reading the html
-                steal.html.wait();
-                
-                $.get(url, {}, this.callback('replaceContent'),"text");
-            },
-            replaceContent : function(html) {
-                this.element.html(html);
-                
-                // indicate the html is ready to be crawled
-                steal.html.ready();
-            }
-            
-        });
-        
-        $(".content").ajaxy();
-        
-    })
+function($){ var lang = $.language();
+    $.getJSON('locales/'+lang+'.json').complete(function(dictionary){
+    
+    // Initialize jSperanto I18n plugin
+    $.jsperanto.init(function(){},{
+        lang: lang, 
+        dictionary: dictionary
+    });
+
+    // Initialize topbar
+    $('.topbar').pachange_topbar();
+
+    $.Controller('Ajaxy',{
+        init : function(){
+            this.updateContent();
+        },
+        "{window} hashchange" : function() {
+            this.updateContent();
+        },
+        updateContent : function(){
+            var hash = window.location.hash.substr(2),
+            url = "fixtures/"+(hash||"home")+".html";
+
+            // postpone reading the html
+            steal.html.wait();
+
+            $.get(url, {}, this.callback('replaceContent'),"text");
+        },
+        replaceContent : function(html) {
+            this.element.html(html);
+
+            // indicate the html is ready to be crawled
+            steal.html.ready();
+        }
+
+    });
+
+    $(".content").ajaxy();
+})});
